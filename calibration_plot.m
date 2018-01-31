e59e90de1161818d5525f3c134b7203d581564dd
HappyZ <zhuyanzi@gmail.com>
2018-01-31 11:03:39 -0600

    remove unnecessary files; add mean/median fitting comparison

--- calibration_plot.m
+++ calibration_plot.m
@@ -3,6 +3,7 @@ clf; hold on;
 
 desired_result = [60:30:1200, 120, 170, 200, 430, 1000, 1300:100:2000];
 median_result = zeros(size(desired_result));
+mean_result = zeros(size(desired_result));
 diff_result = zeros(size(desired_result));
 all_data = [];
 for i = 1:length(desired_result)
@@ -26,6 +27,7 @@ for i = 1:length(desired_result)
 
     cdfplot(data(9, :));
     
+    mean_result(i) = mean(data(9, :));
     median_result(i) = median(data(9, :));
     all_data = [all_data, [data(9, :); dist * ones(1, length(data(9, :)))]];
     diff_result(i) = desired_result(i) - median_result(i);
@@ -37,6 +39,9 @@ params = polyfit(all_data(1, :), all_data(2, :), 1)
 fitted_data = params(1) * all_data(1, :) + params(2);
 params2 = polyfit(median_result, desired_result, 1)
 fitted_data2 = params2(1) * all_data(1, :) + params2(2);
+params3 = polyfit(mean_result, desired_result, 1)
+fitted_data3 = params3(1) * all_data(1, :) + params3(2);
 hold on;
 plot(all_data(1, :), fitted_data, '-')
 plot(all_data(1, :), fitted_data2, '-')
+plot(all_data(1, :), fitted_data3, '-')
